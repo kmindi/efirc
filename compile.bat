b6bcb1a1842d3c95c28e66220ff01f473f3c77aa
fabianruch <fabianruch@ab522a7c-5096-11dc-afc5-cb66f0b61238>
2009-02-27 22:32:19 +0000

    - belanglose Kommentare und Statusmeldungen entfernt - Abfrage, ob efirc.exe (in einem neuen Fenster) gestartet werden soll,  eingebaut - ERRORLEVEL wird nicht mehr ausgegeben, da es so nicht den gewuenschten  Effekt hat
    git-svn-id: svn://svn.code.sf.net/p/efirc/code/trunk@185 ab522a7c-5096-11dc-afc5-cb66f0b61238

--- compile.bat
+++ compile.bat
@@ -1,9 +1,5 @@
 @echo off
 
-rem make wird fuer das aktuelle Makefile
-rem aufgerufen
-echo.
-echo ...calling make...
 mingw32-make.exe 2> build.log
 
 rem ERRORLEVEL abfragen und ggf.
@@ -12,26 +8,17 @@ rem und den Programmaufruf
 rem auslassen
 if ERRORLEVEL 1 goto error
 
-rem keine Fehler
-rem in bin Verzeichnis wechseln
-echo.
 cd bin
 move ..\src\efirc.exe efirc.exe
 
-echo ...Stripping...
 strip --strip-all efirc.exe
 
-rem echo ...c0mprim!sing...
-rem upx --best efirc.exe
-
-rem efirc.exe in bin aufrufen
-echo ...starting...
-efirc.exe
-exit %ERRORLEVEL%
+set /p input="run efirc.exe? ([y]es or any key) "
+if "%input%" == "y" start efirc.exe
+exit
 
 rem Fenster offen halten, um
 rem moegliche Fehler zu analysieren
 :error
-echo.
-echo ERRORLEVEL %ERRORLEVEL% (see build.log for details)
+echo error (see build.log for details)
 pause
