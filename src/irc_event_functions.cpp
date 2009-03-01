2aca047f8a28a95ed79f5dcb7a34e4b5407ba1dd
kmindi <kmindi@ab522a7c-5096-11dc-afc5-cb66f0b61238>
2009-03-01 11:56:38 +0000

    CTCP VERSION Antwort gibt jetzt das aktuelle Betriebssystem zur Laufzeit mit an, nicht mehr nur "Windoofs" ^^.
    git-svn-id: svn://svn.code.sf.net/p/efirc/code/trunk@197 ab522a7c-5096-11dc-afc5-cb66f0b61238

--- src/irc_event_functions.cpp
+++ src/irc_event_functions.cpp
@@ -73,7 +73,7 @@ void irc_pmsg(const irc_msg_data *msg_data, void *cp)
             {
                 // Fenster auswählen
                 wxGetApp().fenstersuchen(empfaenger)->NachrichtAnhaengen(_T("CTCP"),user,ctcp_befehl);
-                wxString version = _T("efirc:") + wxGetApp().efirc_version_string + _T("(") + wxString(wxVERSION_STRING,wxConvUTF8) + _T("):Windoofs");
+                wxString version = _T("efirc:") + wxGetApp().efirc_version_string + _T(" (") + wxString(wxVERSION_STRING,wxConvUTF8) + _T("):") + wxGetOsDescription();
                 wxGetApp().irc->send_ctcp_version(user.mb_str(), version.mb_str());
             }
             else
