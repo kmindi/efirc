8fe432caac3e6f535b201e8a74058924c4de9721
fabianruch <fabianruch@ab522a7c-5096-11dc-afc5-cb66f0b61238>
2009-03-14 13:58:21 +0000

    cppirc-import (r118)
    git-svn-id: svn://svn.code.sf.net/p/efirc/code/trunk@237 ab522a7c-5096-11dc-afc5-cb66f0b61238

--- src/contrib/cppirc/sstring.h
+++ src/contrib/cppirc/sstring.h
@@ -6,7 +6,9 @@
 #if defined WIN32 || linux
 size_t	 strlcat(char *, const char *, size_t);
 size_t	 strlcpy(char *, const char *, size_t);
+#ifdef WIN32
 char	*strsep(char **, const char *);
+#endif
 #endif
 
 void strsub(char **, char **, char);
