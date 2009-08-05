0782b58a1dd920a2a75368a392ced5a68b5268e0
kmindi <kmindi@ab522a7c-5096-11dc-afc5-cb66f0b61238>
2009-08-05 13:09:46 +0000

    Kursiv, Fett und Unterstrichen geschriebene Ausgaben koennen in der Konfiguration festgelegt werden (%italic, %bold, %underlined) Farben werden auch unterstuetzt werden. Issue: 2744177
    git-svn-id: svn://svn.code.sf.net/p/efirc/code/trunk@361 ab522a7c-5096-11dc-afc5-cb66f0b61238

--- src/zentrale.cc
+++ src/zentrale.cc
@@ -166,6 +166,8 @@ void Zentrale::Konfiguration_anpassen()
         config->edit_cfg_replace(_T("%random_string"), zufallstext(4));
         config->edit_cfg_replace(_T("%real_name"), wxGetUserId());
         config->edit_cfg_replace(_T("%italic"), _T("\x0009"));
+        config->edit_cfg_replace(_T("%bold"), _T("\x0002"));
+        config->edit_cfg_replace(_T("%underlined"), _T("\x001F"));
 }
 
 // Funktionen die auf die Fenster-Klasse zugreifen bzw. auf Instanzen eben dieser
