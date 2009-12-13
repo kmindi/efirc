3c1dfad5da1e4b02f02f47303eb8897dd9886b81
kmindi <kmindi@ab522a7c-5096-11dc-afc5-cb66f0b61238>
2009-12-13 13:44:16 +0000

    Um die Version auszulesen gibt es schon das Programm svnversion Svn Datum wird nicht mehr in diesem Script gesetzt.
    git-svn-id: svn://svn.code.sf.net/p/efirc/code/trunk@383 ab522a7c-5096-11dc-afc5-cb66f0b61238

--- src/svnversion.sh
+++ src/svnversion.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # Letzte gesendete Version in $version speichern
-version=`svn info | grep Rev: | awk '{print $4}'`
-# Datum der letzten Version in $datum speichern
-datum=`svn info | grep datum | awk '{print $3" "$4}'`
+version=`svnversion`
 
 #echo $version;
 #echo $datum;
 
 sed -e 's/\$WCREV\$/'$version'/' $1 > $2;
-sed -e 's/\$WCDATE\$/'"$datum"'/' $2 > tmp;
-mv tmp $2;
