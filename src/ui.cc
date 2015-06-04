52a44e39b81990b85935c640a50acffaca154d2d
Kai Mindermann <kai.mindi@gmx.de>
2015-06-04 21:27:18 +0200

    adds new MIT license information to source files

--- src/ui.cc
+++ src/ui.cc
@@ -1,9 +1,9 @@
 /**
-** efirc steht unter der "Creative Commons Namensnennung-Weitergabe unter gleichen Bedingungen 3.0 Deutschland Lizenz".
+** efirc steht unter der MIT-Lizenz.
 ** efirc wird und wurde bis jetzt von Kai Mindermann und Fabian Ruch entwickelt.
 ** Dieser Hinweis darf nicht entfernt werden!
 **
-** efirc is licensed under the "Creative Commons Attribution-Share Alike 3.0 Germany License".
+** efirc is licensed under the MIT License.
 ** efirc was and is developed by Kai Mindermann and Fabian Ruch.
 ** This important information must not be removed!
 **/
